d356d6aae1978a8610f1e5e0e0e225b72d7aa752
Jordan <jordplamondon@gmail.com>
2017-10-17 08:58:21 -0700

    changed css file name, added alert and function to subscribe button

--- README.md
+++
@@ -1,9 +0,0 @@
-# Aloha Project
-
-CSS project 1 RED Academy
-
-## Features
-
-- CSS3
-- Html5
-- jQuery
--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,15 @@ $('.main-carousel').flickity({
   cellAlign: 'left',
   contain: true
 });
+// $('subscribe-input').on('submit', function(event) {
+//   event.preventDefault();
+// if ( $()****finsih this***8
+// });
+$(".submitbutton").on("click",function() {
+var regExp = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+if (regExp.test($('.subscribe-input').val()) ){
+  alert("Thank you for your Subscription");
+} else { 
+  alert("Try again");
+}
+});
